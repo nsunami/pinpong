3cb41f47b5866dadd08c86af829e1c8acd8a1191
Nami Sunami <nsunami@pm.me>
2025-06-09 11:51:09 +0200

    feat: Add the buttons with sounds

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-# sv
+# PinPong Buttons
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
-
-## Creating a project
-
-If you're seeing this, you've probably already done this step. Congrats!
-
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
-```
+````
 
 ## Developing
 
@@ -23,7 +11,7 @@ npm run dev
 
 # or start the server and open the app in a new browser tab
 npm run dev -- --open
-```
+````
 
 ## Building
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let rightSound: HTMLAudioElement | null = null;
+	let wrongSound: HTMLAudioElement | null = null;
+
+	const playRightSound = () => {
+		if (rightSound) return rightSound.play();
+	};
+	const playWrongSound = () => {
+		if (wrongSound) return wrongSound.play();
+	};
+</script>
+
+<!-- Audio element with a reference bound -->
+<audio bind:this={rightSound} src="right.mp3" preload="auto"></audio>
+<audio bind:this={wrongSound} src="wrong.mp3" preload="auto"></audio>
+
+<div class="flex h-svh flex-col items-center justify-center gap-10 bg-slate-800">
+	<!-- Button to trigger the sound effect -->
+
+	<button
+		class="size-30 rounded-full bg-emerald-400 hover:cursor-pointer active:scale-95 active:bg-emerald-700 active:opacity-95"
+		on:click={playRightSound}
+	>
+		<img src="thumbs-up.png" alt="A thumbs-up button" />
+	</button>
+	<button
+		class="size-30 rounded-full bg-red-300 hover:cursor-pointer active:scale-95 active:bg-red-600 active:opacity-95"
+		on:click={playWrongSound}
+	>
+		<img src="thumbs-down.png" alt="A thumbs-down button" />
+	</button>
+</div>
