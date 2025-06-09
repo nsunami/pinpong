be68d990655734027e95dc110a546b76c6878906
Nami Sunami <nsunami@pm.me>
2025-06-09 12:12:34 +0200

    feat: Add styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,19 +14,11 @@
 <audio bind:this={rightSound} src="right.mp3" preload="auto"></audio>
 <audio bind:this={wrongSound} src="wrong.mp3" preload="auto"></audio>
 
-<div class="flex h-svh flex-col items-center justify-center gap-10 bg-slate-800">
+<div class="background flex h-svh flex-col items-center justify-center gap-10 bg-slate-800">
 	<!-- Button to trigger the sound effect -->
 
-	<button
-		class="size-30 rounded-full bg-emerald-400 hover:cursor-pointer active:scale-95 active:bg-emerald-700 active:opacity-95"
-		on:click={playRightSound}
-	>
-		<img src="thumbs-up.png" alt="A thumbs-up button" />
-	</button>
-	<button
-		class="size-30 rounded-full bg-red-300 hover:cursor-pointer active:scale-95 active:bg-red-600 active:opacity-95"
-		on:click={playWrongSound}
-	>
-		<img src="thumbs-down.png" alt="A thumbs-down button" />
+	<button class="button thumbs-up" on:click={playRightSound} aria-label="A thumbs-up button"
+	></button>
+	<button class="button thumbs-down" on:click={playWrongSound} aria-label="A thumbs-down button">
 	</button>
 </div>
